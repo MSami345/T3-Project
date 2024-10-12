b59aa39b39f226fee37707a1b721a742e0864803
Sami <sami03496@gmail.com>
2024-10-12 10:54:12 +0500

    added a post in database

--- src/app/sample-page/page.tsx
+++ src/app/sample-page/page.tsx
@@ -1,15 +1,26 @@
 // 'use client'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import AddPostComp from '~/components/AddPostComp'
 import { db } from '~/server/db'
 
+// export const dynamic = 'force-dynamic'
+export const dynamic = 'force-dynamic'
 
 const page = async () => {
+    const posts = await db.query.posts.findMany()
 
-    const posts = await db.query.posts.findFirst()
+
     // console.log(posts)
     return (
-        <div>
-            {posts?.name}
+        <div className='flex flex-col items-center gap-10'>
+            {/* <p>{process.env.POSTGRES_URL}</p> */}
+            {posts.length && posts.map((post, index) => <p className='text-2xl font-normal' key={index}>
+                name : <strong>{post.name}</strong>
+                <br />
+                <span>created At : <strong>{post.createdAt.toISOString()}</strong></span>
+            </p>)
+            }
+            <AddPostComp />
         </div>
     )
 }
---
+++ src/components/AddPostComp.tsx
@@ -0,0 +1,31 @@
+'use client'
+import React, { useEffect, useState } from 'react'
+import { db } from '~/server/db';
+import { posts } from '~/server/db/schema';
+
+const AddPostComp = () => {
+    const [flag, setFlag] = useState(false);
+
+    const addPost = async () => {
+        try {
+            await db.insert(posts).values({ name: 'saad' })
+        } catch (error) {
+            console.log(error)
+            alert("error adding posts in database")
+        }
+    }
+
+    useEffect(() => {
+        if (flag) {
+            addPost()
+        }
+    }, [flag])
+    return (
+        <div>
+
+            <button onClick={() => setFlag(!flag)}>{flag ? 'created' : 'create'}</button>
+        </div>
+    )
+}
+
+export default AddPostComp
